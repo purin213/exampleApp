98a7ec512b5d696356a93a6a94fa0e2def5e5ff4
Christian <purin213@hotmail.com>
2023-01-30 22:51:02 +0900

    Made the base layer

---
+++ app/components/Details.svelte
@@ -0,0 +1,7 @@
+<page> </page>
+
+<script>
+    import { FlickService } from "~/services/FlickService";
+    export let flickId
+    let flick = FlickService.getInstance().getFlickById(flickId)
+</script>
